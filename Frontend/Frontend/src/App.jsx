2f2efd96435c3f8835aca1176702a48f2497b345
Biman721443 <bimangharai721443@gmail.com>
2025-02-09 03:05:28 +0530

    summit

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import Carousel from "./components/Carousel";
-import Navbar from "./components/Navbar"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdventureTravel from "./components/AdventureTravel"
-import Footer from "./components/Footer"
-import TravelCards1 from './components/TravelCards1';
-import TravelType from "./components/TravelType"
-import CityCards from "./components/CityCards"
-import CountryCards from "./components/CountryCards"
-import AboutUs from "./components/AboutUs"
-import AuthForm from "./components/Signup"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import './App.css';
+
+import Homepage from "./components/Homepage";
 
 function App() {
-
   return (
-    // // <div className="flex items-center justify-center min-h-screen bg-gray-100 w-100%">
-    // //   <Navbar />
-    // //   <Carousel />
-    // // </div>
-    <><Router>
-      {/* <Navbar />
-      <Carousel />
-      <TravelType />
-      <CityCards />
-      <CountryCards />
-      <Footer /> */}
-      <AuthForm/>
+    <Router>
+     <Homepage/>
     </Router>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
